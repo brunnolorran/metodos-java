4ccad623825df7d168a0b93ccd16694dc904f6e0
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-21 11:20:04 -0300

    first commit and created method calculator

---
+++ Calculo.java
@@ -0,0 +1,30 @@
+/**
+ * Calculo
+ */
+public class Calculo {
+
+    // Método que realiza a soma de 2 números
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é: " + resultado);
+    }
+
+    // Método que realiza a soma de 2 números
+    public static void subtrai(double numero1, double numero2) {
+        double resultado = numero1 - numero2;
+        System.out.println("A subtração de " + numero1 + " menos " + numero2 + " é: " + resultado);
+    }
+
+    // Método que realiza a soma de 2 números
+    public static void multiplica(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        System.out.println("A multiplicação de " + numero1 + " e " + numero2 + " é: " + resultado);
+    }
+
+    // Método que realiza a soma de 2 números
+    public static void divide(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+        System.out.println("A divisão de " + numero1 + " e " + numero2 + " é: " + resultado);
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // calculadora
+        Calculo.soma(4, 2);
+        Calculo.subtrai(10, 2);
+        Calculo.multiplica(10, 2);
+        Calculo.divide(10, 2);
+    }
+}
