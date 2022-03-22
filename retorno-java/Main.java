6e5607c53c602e9f741f1e8a785a7499ad3c42e0
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-22 01:27:41 -0300

    criado retorno

---
+++ retorno-java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //retornos
+        double areaQuadrado = QuadrilateroRetorno.area(2);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = QuadrilateroRetorno.area(2, 3);
+        System.out.println("Área do retangulo: " + areaRetangulo);
+
+        double areaTrapezio = QuadrilateroRetorno.area(5, 2, 3);
+        System.out.println("Área do trapezio: " + areaTrapezio);
+        
+        ;
+
+    }
+
+}
