6851d93f42e4dfc9cae924a3b06f1b3907a052a2
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-22 01:07:55 -0300

    criado sobrecarga

---
+++ sobrecarga-java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Quadrilatero.area(2);
+        Quadrilatero.area(2, 3);
+        ;
+        Quadrilatero.area(5, 2, 3);
+        ;
+
+    }
+
+}
