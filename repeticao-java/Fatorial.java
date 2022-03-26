b1badddd949917b6a9c08c7dc2b3b78aaf35c66a
brunnolorran <brunnolorran.silva@gmail.com>
2022-03-26 00:21:55 -0300

    criado ex06

--- repeticao-java/Fatorial.java
+++ repeticao-java/Fatorial.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número
+inteiro fornecido pelo usuário.
+Ex.: 5! = 120
+*/
+
 public class Fatorial {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o número que queira o fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int contador = fatorial;
+        int resultadoFatorial = 1;
+
+        while (contador > 0) {
+            resultadoFatorial = resultadoFatorial * contador;
+            contador--;
+        }
+
+        System.out.println(fatorial + "!= " + resultadoFatorial);
+
+    }
 
 }
