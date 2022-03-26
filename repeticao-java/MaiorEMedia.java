bf6af342e02e64feaf9da256320db2bf1e49305d
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-25 22:53:43 -0300

    criado ex03

--- repeticao-java/MaiorEMedia.java
+++ repeticao-java/MaiorEMedia.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números
+
+*/
+
 public class MaiorEMedia {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int maior = 0;
+        int total = 0;
+        int numero;
+        int contador = 0;
+
+        while (contador < 5) {
+            System.out.println("Digite um número: ");
+            numero = scan.nextInt();
+            if (numero > maior) {
+                maior = numero;
+            }
+            total = total + numero;
+            contador++;
+        }
+
+        double media = total / 5;
+
+        System.out.println("O maior número digitado: " + maior);
+        System.out.println("Total: " + total);
+        System.out.println("A média dos números: " + media);
+
+    }
 
 }
